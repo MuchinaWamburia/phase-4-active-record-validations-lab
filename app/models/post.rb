89589cac9b852d8835df7b40bb79abafc6f5b3cb
MuchinaWamburia <lucymuchina20@gmail.com>
2023-03-17 22:52:41 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :sufficiently_clickbait_y
+
+  private
+
+  def sufficiently_clickbait_y
+    clickbait_phrases = [/Won't Believe/i, /Secret/i, /Top \d+/i, /Guess/i]
+    unless clickbait_phrases.any? { |phrase| phrase.match(title) }
+      errors.add(:title, "must be sufficiently clickbait-y")
+    end
+  end
 end
